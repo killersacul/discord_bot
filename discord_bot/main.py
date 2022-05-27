c3e5fa3ae327509100f918050325bf338956b37e
killersacul <lucas.mouillet@gmail.com>
2022-05-27 16:08:59 +0200

    added basic docker-compose

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,10 +1,9 @@
+import logging
 import os
 import random
 
 from discord.ext import commands
-from dotenv import load_dotenv
 
-load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
